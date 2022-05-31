9c5074ab76811915fa9ab238aef1c2756b236df0
ThePebblemancer <marcelo.alakazan@gmail.com>
2022-05-31 18:28:56 -0300

    Update mod.lua

--- mod.lua
+++ mod.lua
@@ -3,7 +3,7 @@ MOD_NAME = "natura_bees"
 function register()
   return {
     name = MOD_NAME,
-    hooks = {"ready", "gui", "click"}, -- subscribe to hooks we want so they're called
+    hooks = {"click"}, -- subscribe to hooks we want so they're called
     modules = {"utility"} -- load other modules we need, in this case "/modules/utility.lua"
   }
 end
@@ -12,10 +12,15 @@ function init()
 
   api_set_devmode(true)
 
-  log("init", "Hello World!")
+  api_log("init", "Hello World!")
 
-  -- define a new bee species, add a hybrid recipe for it, and add a new trait to all bees
   define_bee()
 
   return "Success"
+end
+
+function click(button)
+
+  api_log("click", button
+
 end
