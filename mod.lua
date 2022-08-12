cf09984538faa2dc0ffbe652b80e0d4c9f47345e
ThePebblemancer <marcelo.alakazan@gmail.com>
2022-08-11 23:58:32 -0300

    v0.1.0
    Added a bunch of trees, mutations for trees and quests

--- mod.lua
+++ mod.lua
@@ -3,8 +3,8 @@ MOD_NAME = "natura_bees"
 function register()
   return {
     name = MOD_NAME,
-    hooks = {"click", "clock", "destroy", "worldgen", "ready"},
-    modules = {"utility", "define", "mutations", "npc", "mutator"}
+    hooks = {"click", "clock", "destroy", "worldgen", "ready", "create"},
+    modules = {"utility", "define", "mutations", "npc", "mutator", "quests"}
   }
 end
 
@@ -25,21 +25,24 @@ function init()
   ready()
 
   destroy()
-  
+  create()
+
   worldgen()
 
   npc_definition()
-  discovery()
+  --discovery()
   --change_stock()
 
   --flower_definition()
 
   menu_objects()
-  seed_exchanger_define()
-  seed_exchanger_draw()
-  seed_exchanger_tick()
-  seed_exchanger_change()
+  --seed_exchanger_define()
+  --seed_exchanger_draw()
+  --seed_exchanger_tick()
+  --seed_exchanger_change()
   --recipes()
+
+  define_quests()
 
   return "Success"
 end
